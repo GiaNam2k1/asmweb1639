eabb5c578aba3a107f57b01a6edd90ebb8585c3b
GiaNam2k1 <94509409+GiaNam2k1@users.noreply.github.com>
2021-12-27 23:36:49 +0700

    ok

---
+++ src/Entity/Course.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CourseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CourseRepository::class)]
+class Course
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Name;
+
+    #[ORM\Column(type: 'integer')]
+    private $CourseID;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getCourseID(): ?int
+    {
+        return $this->CourseID;
+    }
+
+    public function setCourseID(int $CourseID): self
+    {
+        $this->CourseID = $CourseID;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Description): self
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+}
