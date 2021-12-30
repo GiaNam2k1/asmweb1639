01c13f26e0108db60421d93ecdded7c61319dc06
Minh212 <minhhqgch190485@fpt.edu.vn>
2021-12-30 14:07:00 +0700

    minh

--- src/Form/CourseCategoryType.php
+++ src/Form/CourseCategoryType.php
@@ -27,7 +27,16 @@ class CourseCategoryType extends AbstractType
                 'attr' => [
                     'minlength' => 5,
                     'maxlength' => 50
-                ]
+            ]
+            ])
+            ->add('courses', EntityType::class,
+            [
+                'label' => 'Courses',
+                'required' => true,
+                'class' => Course::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true
             ])
         ;
     }
